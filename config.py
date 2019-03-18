dc7617ae21ce49fa75bf112087e604fe25575f7e
frosty-nee <frostbite@frosty-nee.net>
2019-03-18 14:15:10 +0000

    got listing only dockable stations working

--- config.py
+++
@@ -1,1 +0,0 @@
-default_faction = 1000179
--- route.py
+++ route.py
@@ -15,27 +15,30 @@ def fetch_system_data():
 if __name__ == '__main__':
 	conn = sqlite3.connect('sqlite-latest.sqlite')
 	curs = conn.cursor()
-	factions = {}
-	faction_NPC_corp = {'caldari': 1000180, 'minmatar': 1000182, 'gallente': 1000181, 'amarr': 1000179}
+        factions = {'caldari': 500001, 'minmatar': 500002, 'amarr': 500003, 'gallente': 500004}
+	faction_NPC_corp = {500001: 1000180, 500002: 1000182, 500004: 1000181, 500003: 1000179}
 	if config.default_faction == 0:
-		faction = input("FW faction to run missions for: ").lower()
-		if faction in faction_NPC_corp.keys():
-			faction = faction_NPC_corp[faction]
+		mission_group = input("FW faction to run missions for: ").lower()
+		if mission_group in faction_NPC_corp.keys():
+                        faction = factions[mission_group]
+			corp = faction_NPC_corp[faction]
 		else: 
 			print("faction not found")
 			sys.exit()
-	else: faction = config.default_faction
-	curs.execute('''SELECT stationID, solarSystemID FROM staStations where corporationID = ?''', (faction, ))
+	else: 
+            faction = config.default_faction
+            corp = faction_NPC_corp[faction]
+
+        curs.execute('''SELECT stationID, solarSystemID, security FROM staStations where corporationID = ?''', (corp, ))
 	station_list = curs.fetchall()
 	system_data = fetch_system_data()
 	owned_systems = []
 	for system in system_data:
-		if system['occupier_faction_id'] == faction and system['owner_faction_id'] == faction:
+            if system['occupier_faction_id'] == faction:
 			owned_systems.append(system['solar_system_id'])
-	print(owned_systems)
 	available_mission_stations = []
-	for station in station_list: 
-		if station[1] in owned_systems:
+        for station in station_list: 
+		if station[1] in owned_systems or station[2] >= 0.50:
 			available_mission_stations.append(station[0])
-	print(available_mission_stations)
+        print(available_mission_stations)
 
